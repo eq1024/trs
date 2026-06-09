8c7abd27a6ca7779025d37395c3b36257a856a84
kkxx <419315855@qq.com>
2026-06-09 14:21:31 +0800

    feat(config): 更新 Vite 配置以使用 URL 解析别名 feat(i18n): 优化 en-US 和 zh-CN 语言文件结构 chore(lock): 更新 pnpm-lock.yaml 中的 axios 依赖项

--- apps/APP1/vite.config.js
+++ apps/APP1/vite.config.js
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -13,16 +14,18 @@ export default defineConfig(({ mode }) => {
     // 指定加载 .env 文件的目录为 monorepo 的根目录
     envDir: path.resolve(__dirname, '../../'),
     resolve: {
-      alias: isDev
-        ? {
-            '@': path.resolve(__dirname, './src'),
-            '@trs/ui': path.resolve(__dirname, '../../packages/ui/index.ts'),
-            '@trs/utils': path.resolve(__dirname, '../../packages/utils/index.ts'),
-            '@trs/fetch': path.resolve(__dirname, '../../packages/fetch/src/index.ts'),
-            '@trs/i18n': path.resolve(__dirname, '../../packages/i18n/index.ts'),
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        ...(isDev
+          ? {
+              '@trs/ui': path.resolve(__dirname, '../../packages/ui/index.ts'),
+              '@trs/utils': path.resolve(__dirname, '../../packages/utils/index.ts'),
+              '@trs/fetch': path.resolve(__dirname, '../../packages/fetch/src/index.ts'),
+              '@trs/i18n': path.resolve(__dirname, '../../packages/i18n/index.ts'),
             // config 和 lint 包没有 src 入口, 不需要别名
-          }
-        : {},
+            }
+          : {}),
+      },
     },
   }
 })
